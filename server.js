1909babce044d6dacc19d4faec37a2bffab7ebbd
MegMathis <mcdonough.megan25@gmail.com>
2023-04-05 19:19:54 -0400

    server_working

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const PORT = 3000;
+const api_routes = require("./routes/api_routes");
+const view_routes = require("./routes/view_routes");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use('/api', apiRoutes);
+// app.use('/', htmlRoutes);
+app.use("/api", api_routes);
+app.use("/", view_routes);
+
+app.listen(PORT, () => console.log("Listening on port %s", PORT));
